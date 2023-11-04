0c9a8c90c2762844be98c2a14dae883187751b13
ekubyshin <e.kubyshin@gmail.com>
2023-11-04 19:21:38 +0300

    tests

---
+++ internal/storage/file_storage_test.go
@@ -0,0 +1,43 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/ekubyshin/metrics_agent/internal/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRestoreStorage(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"ok",
+			args{
+				"./test/test.json",
+			},
+			2,
+		},
+		{
+			"false",
+			args{
+				"./test/test2.json",
+			},
+			0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := NewMemoryStorage[types.MetricsKey, types.Metrics]()
+			fs, err := NewFileStorage(db, tt.args.filename, true, 0)
+			assert.NoError(t, err)
+			elems := fs.List()
+			assert.Equal(t, tt.want, len(elems))
+		})
+	}
+}
